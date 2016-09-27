391e1106fd01b53b6147dd7fc88bfb9200a69cc4
cs-mac <1014988429@qq.com>
2016-09-27 11:45:49 +0800

    更换module,增加医生端module

--- app/src/main/java/com/medvision/vruser/beans/Message.java
+++
@@ -1,8 +0,0 @@
-package com.medvision.vruser.beans;
-
-/**
- * Created by cs on 16/9/23.
- */
-
-public class Message {
-}
--- app/src/main/java/com/medvision/vruser/database/SpDictionary.java
+++
@@ -1,9 +0,0 @@
-package com.medvision.vruser.database;
-
-/**
- * Created by chenshuai12619 on 2016/3/28 16:19.
- */
-public class SpDictionary {
-	public static final String SP_USER = "user";
-	public static final String TEXT = "text_string";
-}
