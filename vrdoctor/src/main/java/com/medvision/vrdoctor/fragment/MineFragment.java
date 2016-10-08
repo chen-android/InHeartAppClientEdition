448f5cd898776220e759957bfc7b2cc0375ebe48
cs-mac <1014988429@qq.com>
2016-10-08 21:29:02 +0800

    ....

--- vrdoctor/src/main/java/com/medvision/vrdoctor/fragment/MineFragment.java
+++ vrdoctor/src/main/java/com/medvision/vrdoctor/fragment/MineFragment.java
@@ -6,14 +6,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.medvision.vrdoctor.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MineFragment extends Fragment {
 
+
+	@InjectView(R.id.mine_personal_iv)
+	ImageView mMinePersonalIv;
+	@InjectView(R.id.mine_name_tv)
+	TextView mMineNameTv;
+	@InjectView(R.id.mine_phone_tv)
+	TextView mMinePhoneTv;
+	@InjectView(R.id.mine_personal_rl)
+	RelativeLayout mMinePersonalRl;
+	@InjectView(R.id.mine_my_ask_tv)
+	TextView mMineMyAskTv;
+	@InjectView(R.id.mine_my_collection_tv)
+	TextView mMineMyCollectionTv;
+	@InjectView(R.id.mine_my_ask_setting_tv)
+	TextView mMineMyAskSettingTv;
+	@InjectView(R.id.mine_setting_tv)
+	TextView mMineSettingTv;
 
 	public MineFragment() {
 	}
@@ -26,7 +50,30 @@ public class MineFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_mine, container, false);
+		View view = inflater.inflate(R.layout.fragment_mine, container, false);
+		ButterKnife.inject(this, view);
+		return view;
 	}
 
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		ButterKnife.reset(this);
+	}
+
+	@OnClick({R.id.mine_personal_rl, R.id.mine_my_ask_tv, R.id.mine_my_collection_tv, R.id.mine_my_ask_setting_tv, R.id.mine_setting_tv})
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.mine_personal_rl:
+				break;
+			case R.id.mine_my_ask_tv:
+				break;
+			case R.id.mine_my_collection_tv:
+				break;
+			case R.id.mine_my_ask_setting_tv:
+				break;
+			case R.id.mine_setting_tv:
+				break;
+		}
+	}
 }
